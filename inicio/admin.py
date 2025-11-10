051d92c8cec92ae113e53dcf4361fb2e9034a7dc
XOctaviox12 <alfonsosanch123@gmail.com>
2025-11-10 08:40:51 -0600

    mensaje 8

--- inicio/admin.py
+++ inicio/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Descuento, Actividad
+from .models import Descuento, Actividad, Suscripcion
+
+admin.site.site_header = "Panel de Administración - CIPOL"
+admin.site.index_title = "Gestión de Suscripciones y Donaciones"
+admin.site.site_title = "Administrador CIPOL"
 
 @admin.register(Descuento)
 class DescuentoAdmin(admin.ModelAdmin):
@@ -8,3 +12,10 @@ class DescuentoAdmin(admin.ModelAdmin):
 @admin.register(Actividad)
 class ActividadAdmin(admin.ModelAdmin):
     list_display = ('titulo',)
+
+@admin.register(Suscripcion)
+class SuscripcionAdmin(admin.ModelAdmin):
+    list_display = ("user", "amount", "tipo", "status", "created_at")
+    list_filter = ("status", "tipo")
+    search_fields = ("user__username", "stripe_subscription_id")
+    ordering = ("-created_at",)
